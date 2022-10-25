98bb68426d78bd75ea8b9bd14453a66ac14d7d9a
whent666 <89865066+whent666@users.noreply.github.com>
2022-10-25 09:46:31 -0300

    Add files via upload

---
+++ workspace_eclipce/ADS_DesignPatterns/src/state/artigo/EstadoRascunho.java
@@ -0,0 +1,34 @@
+package state.artigo;
+
+import java.time.LocalDateTime;
+
+public class EstadoRascunho implements Estado{
+
+    private Artigo artigo;
+    
+    public EstadoRascunho(Artigo artigo) {
+        this.artigo = new Artigo();
+    }
+    
+    @Override
+    public void publicar() {
+        GerenteDeSeguranca seguranca = GerenteDeSeguranca.getInstance();
+        
+        if(seguranca.ehUsuarioAutor()) {
+            this.artigo.transitarEstadoPara(new EstadoRevisando(artigo));
+            this.artigo.getLogHistorico().add("Transitando para Revisando em " + LocalDateTime.now());
+            return;
+        }else {
+            throw new RuntimeException("Usuario não tem permissão para publicar");
+        }
+        }
+
+
+    @Override
+    public void reprovar() {
+
+    }
+    
+    
+
+}
